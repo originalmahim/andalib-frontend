6e12816bbff7ba8e13bdd45e080482d90f50fecf
originalmahim <tareqazizmahim@gmail.com>
2024-02-07 12:41:35 +0600

    solved some issues successfully

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -1,5 +1,6 @@
-import { FaFacebook, FaGithub, FaTwitter } from 'react-icons/fa';
-// import logo from './../../assets/footerlogo.png'
+import { AiFillFacebook } from "react-icons/ai";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaWhatsappSquare } from "react-icons/fa";
 const Footer = () => {
           return (
             <div className='bg-[#f2f0fd]'>
@@ -8,7 +9,28 @@ const Footer = () => {
           <aside>
           {/* <img className="w-32" src={logo}  /> */}
           <h1 className='text-[30px] '>Andalib</h1>
-          <p>Elevate Your Style, Empower Your Journey. <br/> Curate your fashion story with our exquisite collection .<br/>Where elegance meets functionality .</p>
+          <p className=" text-[16px]">Elevate Your Style, Empower Your Journey. <br/> Curate your fashion story with our exquisite collection .<br/>Where elegance meets functionality .</p>
+          <div className='flex items-center justify-center gap-2'>
+          <a
+            href="/"
+            className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+          >
+            <AiFillFacebook className='text-3xl text-pink-500 '></AiFillFacebook>
+          </a>
+
+          <a
+            href="/"
+            className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+          >
+            <FaTwitterSquare className='text-3xl text-pink-500'></FaTwitterSquare>
+          </a>
+          <a
+            href="/"
+            className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+          >
+            <FaWhatsappSquare className='text-3xl text-pink-500'></FaWhatsappSquare>
+          </a>
+          </div>
           </aside>
           <div className='grid grid-flow-col lg:grid-cols-3 md:grid-cols-3 grid-cols-2 gap-16'>
           <nav className='hidden lg:grid md:grid' >
@@ -38,31 +60,15 @@ const Footer = () => {
           </nav>
             </div> 
           </footer> 
-          <div className="flex flex-col-reverse lg:justify-between  pt-5 pb-10 border-t border-deep-purple-accent-200 sm:flex-row">
+          <div className="flex flex-col-reverse items-center lg:justify-between  pt-5 pb-10 border-t border-deep-purple-accent-200 sm:flex-row">
+            <div>
         <p className="text-[16px] text-black text-center">
         স্বত্ব © ২০২৩ - ২০২৪ আন্দালিব কর্তৃক সর্বস্বত্ব সংরক্ষিত
         </p>
-        <div className="flex items-center justify-center mt-4 mb-4 space-x-4 sm:mt-0">
-
-        <a
-            href="/"
-            className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
-          >
-            <FaGithub className='text-2xl text-black'></FaGithub>
-          </a>
-
-          <a
-            href="/"
-            className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
-          >
-            <FaTwitter className='text-2xl text-black'></FaTwitter>
-          </a>
-          <a
-            href="/"
-            className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
-          >
-            <FaFacebook className='text-2xl text-black'></FaFacebook>
-          </a>
+            </div>
+            
+        <div className="flex items-center justify-center ">
+        <img className='w-60' src="https://pbookbd.com/wp-content/uploads/2022/08/Online.svg" />
         </div>
       </div>
     </div>                            
