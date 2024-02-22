8496eda8ec94c8612b77a88b8359f2f2671f1a18
originalmahim <tareqazizmahim@gmail.com>
2024-02-22 15:23:46 +0600

    Connected Frontend with live backend code successfully

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -14,7 +14,7 @@ const Menu = () => {
     // Fetch data from the backend
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://localhost:5000/bags`);
+        const response = await fetch(`https://task-backend-sigma.vercel.app/bags`);
         const data = await response.json();
         setMenu(data);
         setFilteredItems(data); // Initially, display all items
